1d90316dd52fdd1f3135a9b70be6f8de04358e86
Eduardo Machado <127902640+MachadoEduardo@users.noreply.github.com>
2024-03-07 17:06:22 -0300

    Update verificarIntervalo.c

--- verificarIntervalo.c
+++ verificarIntervalo.c
@@ -1,6 +1,7 @@
 /* Escreva um programa em linguagem C que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0, 25], (25, 50], (50, 75], (75, 100]) este valor se encontra. Se o valor não estiver em nenhum
 destes intervalos, deverá ser impressa a mensagem "Fora de intervalo". Observação: o símbolo "(" representa "maior que". Por exemplo, [0, 25] indica valores entre 0 . 25.00000, inclusive eles. Já (25, 50] indica valores maiores
 que 25.00000 até o valor 50.00000.*/
+/* Beecrowd: Intervalo (1037) */
 
 #include <stdio.h>
 
