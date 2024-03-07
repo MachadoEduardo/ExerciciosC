d3c03e557afdf69311f6a80d335683f9d6277f0f
Eduardo Machado <127902640+MachadoEduardo@users.noreply.github.com>
2024-03-07 16:50:31 -0300

    Create conversaoTempo.c

---
+++ conversaoTempo.c
@@ -0,0 +1,16 @@
+/* Crie um programa na linguagem C que receba como entrada um valor inteiro, que é o tempo de duração em segundos de um evento em uma fábrica, e informe-o expresso no formato "horas:minutos:segundos". */
+
+#include <stdio.h>
+
+int main()
+{
+    int n, horas, minutos, segundos;
+    
+    scanf("%d", &n);
+    
+    segundos = n % 60;
+    minutos = (n / 60) % 60;
+    horas = n / 3600;
+    
+    printf("%02d:%02d:%02d", horas, minutos, segundos);
+}
