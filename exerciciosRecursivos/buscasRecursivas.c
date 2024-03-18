41c5ab28ad0ab474db373d336740372cef2b1bf9
Eduardo Machado <127902640+MachadoEduardo@users.noreply.github.com>
2024-03-18 20:35:18 -0300

    Update buscasRecursivas.c

--- exerciciosRecursivos/buscasRecursivas.c
+++ exerciciosRecursivos/buscasRecursivas.c
@@ -2,11 +2,80 @@
 #include <time.h>
 #include <stdlib.h>
 
+void funcaoSwap(int *x, int *y);
+int buscaPosMenor(int *v, int inicio, int fim);
+void bubbleSort(int *v, int ini, int fim);
+void insertionSort(int *v, int ini, int fim);
+void selectionSort(int *v, int ini, int fim);
+int buscaSequencialRec(int *v, int ini, int fim, int k);
+int buscaSequencial(int *v, int ini, int fim, int k);
+int buscaBinariaRec(int *v, int ini, int fim, int k);
+int buscaBinaria(int *v, int ini, int fim, int k);
+
+int main(void){
+    int n, *vetor, i;
+
+    scanf("%d", &n);
+
+    vetor = (int *) malloc(sizeof(int)*n);
+    srand(time(NULL));
+
+    for( i = 0; i < n; i++){
+        vetor[i] = rand() % 10 + 1;
+    }
+
+    for( i = 0; i < n; i++){
+        printf("%d ", vetor[i]);
+    }
+    
+    insertionSort(vetor, 0, n - 1);
+    printf("\n");
+    
+    for( i = 0; i < n; i++){
+        printf("%d ", vetor[i]);
+    }
+
+   /* int valor = rand() % n + 1;
+
+    int posicao = buscaBinariaRec(vetor, 0, n - 1, n);
+
+    if(posicao != -1)
+        printf("\nvetor[%d] = %d\n", posicao, vetor[posicao]);
+    else
+        printf("%d nao existe no vetor!\n", valor); */
+
+    return 0;
+}
+
 void funcaoSwap(int *x, int *y){
     int aux;
     aux = *x;
     *x = *y;
     *y = aux;
+}
+
+int buscaPosMenor(int *v, int inicio, int fim){
+    int menor = v[inicio];
+    int posicaoMenor = inicio; 
+    for(int i = inicio; i <= fim; ++i){
+        if(v[i] < menor){
+            menor = v[i];
+            posicaoMenor = i;
+        }
+    }   
+    return posicaoMenor; 
+}
+
+void insertionSort(int *v, int ini, int fim){
+    int i, j, aux;
+    for(i = ini + 1; i <= fim; ++i){
+        aux = v[i];
+        j = i - 1;
+        while(j >= ini && aux < v[j]){
+            funcaoSwap(&v[j + 1], &v[j]);
+            j = j - 1;
+        }
+    }
 }
 
 void bubbleSort(int *v, int ini, int fim){
@@ -17,6 +86,14 @@ void bubbleSort(int *v, int ini, int fim){
                 funcaoSwap(&v[j], &v[j+1]);
             }
         }
+    }
+}
+
+void selectionSort(int *v, int ini, int fim){
+    int i, menor;
+    for(i = ini; i <= fim; ++i){
+        menor = buscaPosMenor(v, i, fim);
+        funcaoSwap(&v[i], &v[menor]);
     }
 }
 
@@ -77,39 +154,4 @@ int buscaBinaria(int *v, int ini, int fim, int k){
         }
     }
     return -1;
-}
-
-int main(void){
-    int n, *vetor, i;
-
-    scanf("%d", &n);
-
-    vetor = (int *) malloc(sizeof(int)*n);
-    srand(time(NULL));
-
-    for( i = 0; i < n; i++){
-        vetor[i] = rand() % 10 + 1;
-    }
-
-    for( i = 0; i < n; i++){
-        printf("%d ", vetor[i]);
-    }
-    
-    bubbleSort(vetor, 0, n - 1);
-    printf("\n");
-    
-    for( i = 0; i < n; i++){
-        printf("%d ", vetor[i]);
-    }
-
-   /* int valor = rand() % n + 1;
-
-    int posicao = buscaBinariaRec(vetor, 0, n - 1, n);
-
-    if(posicao != -1)
-        printf("\nvetor[%d] = %d\n", posicao, vetor[posicao]);
-    else
-        printf("%d nao existe no vetor!\n", valor); */
-
-    return 0;
 }
